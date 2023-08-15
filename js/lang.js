34b6b845b325c15d388b2a8fef85b28e4101155a
nspagliardi <nspagliardi@eiwa.ag>
2023-08-15 14:05:58 -0300

    fix ingress lang

--- js/lang.js
+++ js/lang.js
@@ -1,33 +1,41 @@
 function getUserCountry() {
-  return fetch('https://ipapi.co/json/')
-    .then(response => response.json())
-    .then(data => data.country_code)
-    .catch(error => {
-      console.error('Error fetching user country:', error);
+  return fetch("https://ipapi.co/json/")
+    .then((response) => response.json())
+    .then((data) => data.country_code)
+    .catch((error) => {
+      console.error("Error fetching user country:", error);
       return null;
     });
 }
 
 function redirectToCountryURL(countryCode) {
   switch (countryCode) {
-    case 'ES':
-    case 'MX':
-    case 'AR':
-    case 'CO':
-      window.location.href = 'https://spagliardinicolas.ar/';
+    case "ES":
+    case "MX":
+    case "AR":
+    case "CO":
+      window.location.href = "https://spagliardinicolas.ar/";
       break;
-    case 'IT':
-      window.location.href = 'https://spagliardinicolas.ar/it';
+    case "IT":
+      window.location.href = "https://spagliardinicolas.ar/it";
       break;
     default:
-      window.location.href = 'https://spagliardinicolas.ar/en';
+      window.location.href = "https://spagliardinicolas.ar/en";
+  }
+
+  localStorage.setItem("languageDetected", "true");
+  if (!localStorage.getItem("languageRedirected")) {
+    localStorage.setItem("languageRedirected", "true");
+    window.location.href = redirectURL;
   }
 }
 
-getUserCountry().then(countryCode => {
-  if (countryCode) {
-    redirectToCountryURL(countryCode);
-  } else {
-    console.error('Not found.');
-  }
-});
+if (!localStorage.getItem("languageDetected")) {
+  getUserCountry().then((countryCode) => {
+    if (countryCode) {
+      redirectToCountryURL(countryCode);
+    } else {
+      console.error("Not found.");
+    }
+  });
+}
